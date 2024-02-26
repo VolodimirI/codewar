ded318dd17b9ff143c7935882c2bcc0c1ef24d2d
Volodymyr <publika1812@gmai.com>
2024-02-26 21:45:43 +0200

    com7

---
+++ 8kyu/fixcode.js
@@ -0,0 +1,20 @@
+// function rainAmount(mm)
+// {
+//      if (mm <= 40) {
+//          return `You need to give your plant ${40 - mm}mm of water`;
+//     }
+//     else {
+//          return (`Your plant has had more than enough water for today!`);
+//     }
+// }
+// console.log(rainAmount(30));
+
+function alphabetPosition (b){
+
+
+}
+let text = "HELLO WORLD";
+let code = text.charCodeAt(0);
+console.log(code);
+
+alphabetPosition("The sunset sets at twelve o' clock.")
