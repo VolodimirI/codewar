58331c4ace6ab9b1c5f60fd4c99edc23faa4da0a
Volodymyr <publika1812@gmai.com>
2024-02-26 21:51:02 +0200

    com8

--- 8kyu/fixcode.js
+++ 8kyu/fixcode.js
@@ -1,20 +1,10 @@
-// function rainAmount(mm)
-// {
-//      if (mm <= 40) {
-//          return `You need to give your plant ${40 - mm}mm of water`;
-//     }
-//     else {
-//          return (`Your plant has had more than enough water for today!`);
-//     }
-// }
-// console.log(rainAmount(30));
-
-function alphabetPosition (b){
-
-
+function rainAmount(mm)
+{
+     if (mm <= 40) {
+         return `You need to give your plant ${40 - mm}mm of water`;
+    }
+    else {
+         return (`Your plant has had more than enough water for today!`);
+    }
 }
-let text = "HELLO WORLD";
-let code = text.charCodeAt(0);
-console.log(code);
-
-alphabetPosition("The sunset sets at twelve o' clock.")
+console.log(rainAmount(30));
