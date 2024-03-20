b6cf532e3918108f515ed4951451779d542fd82d
Volodymyr <publika1812@gmai.com>
2024-03-20 19:16:23 +0200

    evenOrOdd-makeNegative

---
+++ 8kyu/negative.js
@@ -0,0 +1,8 @@
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+function makeNegative(num) {
+    // Code?
+
+    return num > 0 ? -num:num;
+}
+console.log(makeNegative(-6));
