9c23cf68ea98f5fa8d8a76d430971601ceeb82cc
Volodymyr <publika1812@gmai.com>
2024-07-05 17:29:18 +0300

    text

--- 8kyu/fakeBin.js
+++ 8kyu/fakeBin.js
@@ -8,7 +8,7 @@ function fakeBin(x){
 }
 // function fakeBin(x){
 //  return x.replace(/[1-4]/g,'0').replace(/[5-9]/g,'1');
-// }
+//}
 console.log(fakeBin("45385593107843568")); //01011110001100111
 console.log(fakeBin("509321967506747")); //101000111101101
 console.log(fakeBin("366058562030849490134388085")); //011011110000101010000011011
